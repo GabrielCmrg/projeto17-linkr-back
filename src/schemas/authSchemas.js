914b7c96f769644b89bdb622b937affe66ef65bd
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-08-09 12:45:22 -0300

    feat: add new controller to register user
    This controller uses the middleware that validates signup body.
    It also uses the users repository to create a new user.
    If the repository returns an error, it responds with 500 or 409 if the error is unique key violation.
    Also changes the schema to use camel case instead of snake case, snake case is used only on repositories.

--- src/schemas/authSchemas.js
+++ src/schemas/authSchemas.js
@@ -10,5 +10,5 @@ export const signupSchema = joi.object({
     )
     .required(),
   name: joi.string().trim().required(),
-  pic_url: joi.string().trim().uri().required(),
+  picUrl: joi.string().trim().uri().required(),
 });
