07b0650b9a7b95d07aa193cc56b79e217c4d0c7a
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-08-10 13:26:25 -0300

    feat: add middleware to validate login object
    Also changes the message sent as response to english.

--- src/middlewares/authMiddlewares.js
+++ src/middlewares/authMiddlewares.js
@@ -4,12 +4,28 @@ export const checkSignupBody = (req, res, next) => {
   const validation = authSchemas.signupSchema.validate(req.body);
   if (validation.error) {
     const returnObject = {
-      message: 'O objeto enviado não é válido. Consulte a documentação da API.',
+      message:
+        "The signup object sent is not valid. See the API's documentation.",
       errors: validation.error,
     };
     return res.status(422).json(returnObject);
   }
 
   res.locals.signupInfo = validation.value;
+  return next();
+};
+
+export const checkLoginBody = (req, res, next) => {
+  const validation = authSchemas.loginSchema.validate(req.body);
+  if (validation.error) {
+    const returnObject = {
+      message:
+        "The login object sent is not valid. See the API's documentation.",
+      errors: validation.error,
+    };
+    return res.status(422).json(returnObject);
+  }
+
+  res.locals.credentials = validation.value;
   return next();
 };
