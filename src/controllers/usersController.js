2fa50baecd3bbe680a7eda8aedf9b515bf985d73
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-08-10 13:59:48 -0300

    feat: add controller to login user
    This controller gets the user by the email passed as credentials.
    If the user does not exist or the password does not match the one on the database return 401.
    Its important to keep the if statement in this order, because it will test the user existance first.
    It also uses JWT to create a token based on the user id and return it.

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,10 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
 import { usersRepository } from '../repositories/index.js';
+
+dotenv.config();
 
 export const registerNewUser = async (req, res) => {
   const { signupInfo } = res.locals;
@@ -8,14 +12,33 @@ export const registerNewUser = async (req, res) => {
   const password = bcrypt.hashSync(receivedPassword, 10);
   try {
     await usersRepository.createUser({ email, name, picUrl, password });
-    return res.status(201).send('Usuário criado!');
+    return res.status(201).send('User created!');
   } catch (error) {
     if (error.routine === '_bt_check_unique') {
-      return res.status(409).send('Esse email já está sendo usado.');
+      return res.status(409).send('This email is already in use.');
     }
     console.error(error);
     return res
       .status(500)
-      .send('Algo deu errado ao tentar registrar o usuário.');
+      .send('Something went wrong when trying to create the user.');
+  }
+};
+
+export const loginUser = async (req, res) => {
+  const { credentials } = res.locals;
+  const { email, password } = credentials;
+  try {
+    const user = await usersRepository.getUserByEmail(email);
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return res.status(401).send('Wrong e-mail or password.');
+    }
+    const { JWT_SECRET_KEY } = process.env;
+    const token = jwt.sign({ userId: user.id }, JWT_SECRET_KEY);
+    return res.json({ token });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .send('Something went wrong when trying to search the user.');
   }
 };
