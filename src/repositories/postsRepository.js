ee4459e16a70419b7420aa38b7c75658fb221827
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-08-11 12:51:24 -0300

    refactor: change all functions to be arrow functions

--- src/repositories/postsRepository.js
+++ src/repositories/postsRepository.js
@@ -1,43 +1,55 @@
 import connection from '../databases/postgres.js';
 
-export async function getPost() {
-  const { rows: posts } = await connection.query(`
-  SELECT posts.id, users.name, users.pic_url, posts.content, urls.url as link_url, urls.title as link_title, urls.image as link_image, urls.description as link_description 
-  FROM posts
-  JOIN users ON users.id = posts.author_id
-  JOIN urls ON posts.url_id = urls.id
-  ORDER BY id DESC
-  LIMIT 20;`);
+export const getPost = async () => {
+  const { rows: posts } = await connection.query(
+    `
+      SELECT 
+        posts.id,
+        users.name,
+        users.pic_url,
+        posts.content,
+        urls.url as link_url,
+        urls.title as link_title,
+        urls.image as link_image,
+        urls.description as link_description 
+      FROM posts
+      JOIN users ON users.id = posts.author_id
+      JOIN urls ON posts.url_id = urls.id
+      ORDER BY id DESC
+      LIMIT 20;
+    `
+  );
 
   return posts;
-}
+};
 
-export async function savePostInDatabase(userId, content, urlId) {
-  return await connection.query(
+export const savePostInDatabase = async (userId, content, urlId) => {
+  await connection.query(
     `
-        INSERT INTO posts(author_id,content,url_id)
-        VALUES ($1,$2,$3)
+      INSERT INTO posts(author_id, content, url_id)
+      VALUES ($1, $2, $3)
     `,
     [userId, content, urlId]
   );
-}
+};
 
-export async function saveUrlInDatabase(url, title, image, description) {
-  return await connection.query(
+export const saveUrlInDatabase = async (url, title, image, description) => {
+  await connection.query(
     `
-        INSERT INTO urls(url, title, image, description)
-        VALUES ($1,$2,$3,$4)
+      INSERT INTO urls(url, title, image, description)
+      VALUES ($1, $2, $3, $4)
     `,
     [url, title, image, description]
   );
-}
+};
 
-export async function getUrl(url) {
-  const { rows: urls } = await connection.query(`
-  SELECT * FROM urls
-  WHERE url = $1;
-  `,
+export const getUrl = async (url) => {
+  const { rows: urls } = await connection.query(
+    `
+      SELECT * FROM urls
+      WHERE url = $1;
+    `,
     [url]
   );
   return urls;
-}
+};
