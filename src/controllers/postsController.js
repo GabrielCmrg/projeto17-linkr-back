13d1d413520615397a14b5b6a5d73bc0c3274951
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-08-11 20:34:58 -0300

    feat: process the hashtags inside content.
    The sendPost controller now analyzes the content to create the hashtags and make relationships.

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,6 +1,42 @@
 import urlMetadata from 'url-metadata';
 
-import { postsRepository, usersRepository } from '../repositories/index.js';
+import {
+  postsRepository,
+  usersRepository,
+  tagsRepository,
+} from '../repositories/index.js';
+
+// Locally used functions
+
+const getUrlMetadata = async (link) => {
+  const { url, title, image, description } = await urlMetadata(link, {
+    descriptionLength: 50,
+  });
+  let urlInfo = await postsRepository.getUrlByUrl(url);
+  if (!urlInfo) {
+    urlInfo = await postsRepository.createUrl(url, title, image, description);
+  }
+
+  return urlInfo;
+};
+
+const extractTags = (content) => {
+  const tags = content.match(/#\w+/g);
+  return tags;
+};
+
+const processTags = async (tags, postId) => {
+  const promises = tags.map(async (tag) => {
+    let tagInfo = await tagsRepository.getTagByName(tag);
+    if (!tagInfo) {
+      tagInfo = await tagsRepository.createTag(tag);
+    }
+    await tagsRepository.createTagMention(tagInfo.id, postId);
+  });
+  await Promise.all(promises);
+};
+
+// Globally used functions
 
 export const getTimelinePosts = async (req, res) => {
   try {
@@ -16,16 +52,16 @@ export const getTimelinePosts = async (req, res) => {
 
 export const sendPost = async (req, res) => {
   const { content, postLink } = res.locals.post;
-  const { url, title, image, description } = await urlMetadata(postLink, {
-    descriptionLength: 50,
-  });
   const { userId } = res.locals;
   try {
-    let urlInfo = await postsRepository.getUrlByUrl(url);
-    if (!urlInfo) {
-      urlInfo = await postsRepository.createUrl(url, title, image, description);
-    }
-    await postsRepository.createPost(userId, content, urlInfo.id);
+    const urlInfo = await getUrlMetadata(postLink);
+    const createdPost = await postsRepository.createPost(
+      userId,
+      content,
+      urlInfo.id
+    );
+    const tags = extractTags(content);
+    await processTags(tags, createdPost.id);
     return res.status(201).send('Post created!');
   } catch (error) {
     console.log(error);
