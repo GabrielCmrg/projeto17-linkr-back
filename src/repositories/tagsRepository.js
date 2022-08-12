c05fd734ed71e45e48acd5d19ee82a1e027c675f
PauloGabrieel <gabrieeel.c@gmail.com>
2022-08-12 17:27:22 -0300

    fix: change tag variable name to tags

--- src/repositories/tagsRepository.js
+++ src/repositories/tagsRepository.js
@@ -24,11 +24,11 @@ export const getTagByName = async (tagName) => {
 };
 
 export const getTags = async () => {
-  const { rows: tag } = await connection.query(`
+  const { rows: tags } = await connection.query(`
   SELECT tags.name, COUNT(tag_mentions.id) AS total 
   FROM tags JOIN tag_mentions ON tags.id=tag_mentions.tag_id 
   GROUP BY tags.name 
   ORDER BY total DESC
   `);
-  return tag;
+  return tags;
 };
