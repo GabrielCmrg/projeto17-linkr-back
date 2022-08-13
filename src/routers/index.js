ebc8388c268e1417b1c8078b08ba28064ac611d5
Victor <victorgpj25@gmail.com>
2022-08-13 20:09:35 -0300

    feat: searchUsers route

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,11 +3,13 @@ import express from 'express';
 import authRouter from './authRouter.js';
 import postsRouter from './postsRouter.js';
 import hashtagsRouter from './hashtagsRouter.js';
+import userRouter from './userRouter.js';
 
 const router = express.Router();
 
 router.use(authRouter);
 router.use(postsRouter);
 router.use(hashtagsRouter);
+router.use(userRouter);
 
 export default router;
