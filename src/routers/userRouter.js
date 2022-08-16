807f7ba1a071e32c22a43b272c3029c12ec7ec08
Victor <victorgpj25@gmail.com>
2022-08-16 12:21:46 -0300

    feat: follow user route

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,12 +1,19 @@
 import express from 'express';
 
 import { usersController } from '../controllers/index.js';
+import { authMiddlewares } from '../middlewares/index.js'
 
 const userRouter = express.Router();
 
 userRouter.post(
   '/searchuser',
   usersController.getUsersByName
+);
+
+userRouter.post(
+  '/followuser',
+  authMiddlewares.tokenValidation,
+  usersController.followUser,
 );
 
 export default userRouter;
