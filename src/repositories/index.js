f3a149f29c712cde95fb4a9e42ed5ee15b5c57bf
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-08-18 22:12:01 -0300

    fix: missing index call

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -2,3 +2,4 @@ export * as usersRepository from './usersRepository.js';
 export * as postsRepository from './postsRepository.js';
 export * as tagsRepository from './tagsRepository.js';
 export * as urlsRepository from './urlsRepository.js';
+export * as commentsRepository from './commentsRepository.js';
